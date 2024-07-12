a0c36f0f199852ee42776b004ea48370a6493f9a
Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>
2024-07-12 00:54:03 -0300

    [LOGS] - Added logs

--- src/main/kotlin/application/usecases/WordCounterUseCase.kt
+++ src/main/kotlin/application/usecases/WordCounterUseCase.kt
@@ -1,13 +1,17 @@
 package application.usecases
 
 import application.adapter.controller.dto.TextRequest
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class WordCounterUseCase {
+    private val logger = LoggerFactory.getLogger(WordCounterUseCase::class.java)
 
     fun execute(text: TextRequest): Map<String, Int> {
+        logger.info("WordCounterUseCase: text = $text")
+
         return text.text.lowercase(Locale.getDefault())
             .split("\\s+".toRegex())
             .filter { it.isNotBlank() }
