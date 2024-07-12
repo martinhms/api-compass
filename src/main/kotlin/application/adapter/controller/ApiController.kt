58d75d37498f7f68a3ff094b90e5c543d3ba3c1f
Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>
2024-07-11 23:53:16 -0300

    [WORD-COUNTER] - Added wordCounter

--- src/main/kotlin/application/adapter/controller/ApiController.kt
+++ src/main/kotlin/application/adapter/controller/ApiController.kt
@@ -1,8 +1,9 @@
 package application.adapter.controller
 
-import application.adapter.controller.dto.Every10thCharacterRequest
+import application.adapter.controller.dto.TextRequest
 import application.adapter.controller.dto.ExtractResponse
 import application.usecases.Every10thCharacterUseCase
+import application.usecases.WordCounterUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class ApiController(
     private val every10thCharacterUseCase: Every10thCharacterUseCase,
+    private val wordCounterUseCase: WordCounterUseCase
 ) {
 
     @PostMapping("/every10thCharacter")
-    suspend fun every10thCharacter(@RequestBody request: Every10thCharacterRequest): ExtractResponse = withContext(Dispatchers.IO) {
+    suspend fun every10thCharacter(@RequestBody request: TextRequest): ExtractResponse = withContext(Dispatchers.IO) {
         val result = every10thCharacterUseCase.execute(request.text)
         ExtractResponse(result)
+    }
+
+    @PostMapping("/wordCounter")
+    suspend fun wordCounter(@RequestBody request: TextRequest): Map<String, Int> = withContext(Dispatchers.IO) {
+        wordCounterUseCase.execute(request)
     }
 }
