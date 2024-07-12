013a20ad1dca9b7156a64f679abc41e23fede70a
martinhms <martinhms2015@gmail.com>
2024-07-12 00:54:47 -0300

    [LOGS] - Added logs (#4)
    Co-authored-by: Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>

--- src/main/kotlin/application/usecases/Every10thCharacterUseCase.kt
+++ src/main/kotlin/application/usecases/Every10thCharacterUseCase.kt
@@ -1,11 +1,14 @@
 package application.usecases
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class Every10thCharacterUseCase {
+    private val logger = LoggerFactory.getLogger(Every10thCharacterUseCase::class.java)
 
     fun execute(text: String): List<Char> {
+        logger.info("Every10thCharacterUseCase: test = $text")
         return text.filterIndexed { index, _ -> (index + 1) % 10 == 0 }.toList()
     }
 }
